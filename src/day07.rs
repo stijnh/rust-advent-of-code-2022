6b5cd5197d1e5c3a3fd26228d652f17df58c557d
stijn <s.heldens@esciencecenter.nl>
2022-12-13 16:25:18 +0100

    Add solution for day 7

--- src/day07.rs
+++ src/day07.rs
@@ -1,7 +1,37 @@
 use crate::common::*;
 
 pub(crate) fn run(lines: Lines) -> Result {
-    todo!()
+    let mut path = vec![];
+    let mut sizes = HashMap::<_, i32>::default();
+
+    for &line in lines {
+        if line == "$ cd /" {
+            path = vec![];
+        } else if line == "$ cd .." {
+            path.pop();
+        } else if let Some(matches) = find_regex("\\$ cd ([a-z]+)", line) {
+            path.push(matches[1].to_string());
+        } else if let Some(matches) = find_regex("([0-9]+) [a-z.]+", line) {
+            let size = matches[1].parse::<i32>().unwrap();
+
+            for i in 0..=path.len() {
+                *sizes.entry(path[..i].to_vec()).or_default() += size;
+            }
+        }
+    }
+
+    let a = sizes.values().filter(|&&e| e <= 100000).sum::<i32>();
+    println!("part A: {}", a);
+
+    let capacity = 70000000;
+    let required = 30000000;
+    let used = sizes[&Vec::<String>::new()];
+    let threshold = required + used - capacity;
+
+    let b = sizes.values().filter(|&&e| e >= threshold).min();
+    println!("part B: {:?}", b);
+
+    Ok(())
 }
 
 #[cfg(test)]
